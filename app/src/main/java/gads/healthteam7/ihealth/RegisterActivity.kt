e521320ca4d84eb7afce3efee7c1b6e11e4fc5a9
sirsamuelayodele <asatechafrica@gmail.com>
2022-11-23 04:13:22 +0100

    Update UI in progress Login and signup

---
+++ app/src/main/java/gads/healthteam7/ihealth/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package gads.healthteam7.ihealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
